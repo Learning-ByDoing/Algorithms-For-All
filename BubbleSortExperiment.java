8752df25b6c57c28b39ca3866efab9090c48693a
Learning-ByDoing <learningbydoing0@gmail.com>
2016-06-03 23:55:56 +0530

    Create BubbleSortExperiment.java

---
+++ BubbleSortExperiment.java
@@ -0,0 +1,77 @@
+class BubbleSort
+{
+    void bubbleSort(int arr[])
+    {
+      
+            boolean swapped = true;
+            int i = 0;
+            int tmp;
+            while (swapped) {
+                  swapped = false;
+                  i++;
+                  for (int j = 0; j < arr.length - i; j++) {                                       
+                        if (arr[j] > arr[j + 1]) {                          
+                              tmp = arr[j];
+                              arr[j] = arr[j + 1];
+                              arr[j + 1] = tmp;
+                              swapped = true;
+                        }
+                  }                
+            }
+      
+    }
+    void bubbleSort1(int arr[])
+    {         
+	    int tmp;
+	      for(int i=0;i<arr.length-1;i++){
+	          for (int j = 0; j < arr.length-i-1; j++) {                                       
+	                if (arr[j] > arr[j + 1]) {                          
+	                      tmp = arr[j];
+	                      arr[j] = arr[j + 1];
+	                      arr[j + 1] = tmp;
+	                }
+	          }   
+	      }
+    }
+      
+    
+ 
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 25, 12, 22, 11};
+        int arr1[] = {64, 25, 12, 22, 11};
+        
+        long startTime = System.nanoTime();
+    	System.out.println(startTime);
+    	
+        ob.bubbleSort(arr);
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+        
+        long totalTime = System.nanoTime() - startTime;
+    	System.out.println(totalTime);
+    	
+    	startTime = System.nanoTime();
+    	System.out.println(startTime);
+    	
+        ob.bubbleSort1(arr1);
+        ob.bubbleSort1(arr1);
+        System.out.println("Sorted array");
+        ob.printArray(arr1);        
+        
+        totalTime = System.nanoTime() - startTime;
+    	System.out.println(totalTime);
+    }
+}
